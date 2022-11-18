93d9707e5789d6e260588a46d20c137d38542bb9
Siva Ram Reddy Kalluri <sivaramreddy.kalluri@impetus.com>
2022-11-18 22:49:19 +0530

    updated lamndafile

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 # Lambda function ARN
 output "lambda_arn" {
-  value = aws_lambda_function.api_lambda.arn
+  value = aws_lambda_function.lambda_fn.arn
 }
 
 # Invoke ARN
 output "invoke_arn" {
-  value = aws_lambda_function.api_lambda.invoke_arn
+  value = aws_lambda_function.lambda_fn.invoke_arn
 }
 
 # Lambda Function Name
 output "function_name" {
   description = "Name of the Lambda function."
-  value = aws_lambda_function.api_lambda.function_name
+  value = aws_lambda_function.lambda_fn.function_name
 }
